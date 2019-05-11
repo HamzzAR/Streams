a2a68f39c666da548dadd8188abfcfa4694023e2
HamzzAR <hamza_razeq@hotmail.co.uk>
2019-05-11 16:21:36 +0100

    added two methods to Family class

--- familyTask/Family.java
+++ familyTask/Family.java
@@ -4,6 +4,41 @@ import java.util.HashMap;
 
 public class Family {
 
+	ArrayList<People> people = new ArrayList<People>();
+
+	HashMap<Object, Object> relationships = new HashMap<Object, Object>();
+	
+
+	public boolean male(String name) {
+		boolean res = false;
+		for (People m : people) {
+			if(m.getName().equals(name)){
+				m.setGender("male");
+				res = true;
+				break;
+			}else{
+				res = false;
+			}
+		}
+		return res;
+	}
+	
+	public boolean female(String name) {
+		boolean res = false;
+		for (People m : people) {
+			if(m.getName().equals(name)){
+				m.setGender("female");
+				res = true;
+				break;
+			}else{
+				res = false;
+			}
+		}
+		return res;
+	}
+	
+
+
 	public static void main(String[] args) {
 		new Family();
 	}
