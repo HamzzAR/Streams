9eac6c0ffcf1f94c314d16fffc5cf8fede0da93c
HamzzAR <hamza_razeq@hotmail.co.uk>
2019-05-13 11:48:04 +0100

    replaceLetterWithPosition method created in AlphabetPosition class and all tests passed for this task

--- src/AlphabetPosition.java
+++ src/AlphabetPosition.java
@@ -5,11 +5,15 @@ import java.util.stream.Collectors;
 
 public class AlphabetPosition {
 	
+		public StringBuilder replaceLetterWithPosition(String string) {
+			StringBuilder sb = new StringBuilder();
+			string.toUpperCase()
+			.chars()
+			.map(x -> (int) x-64)
+			.filter(x -> (x>=1 && x<=26))
+			.forEach(x -> sb.append(" "+x));
+			return sb;
+		}
 
-	public StringBuilder replaceLetterWithPosition(String string) {
-		
-		StringBuilder StringBuilder = new StringBuilder();
-		return StringBuilder;
-	}
 
 }
